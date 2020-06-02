e11539f4e367720c9ff1e1d5241853258c219d31
Haşim ATALAY <hasim@verify.com.tr>
2020-06-02 11:59:02 +0300

    add /merkur endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/merkur', function (req, res) {
+  res.send('Hello Merkur!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
